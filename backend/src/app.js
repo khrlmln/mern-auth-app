2900c5021044870986fe48918090b50935e641c9
Milan Kharel <milankharel454@gmail.com>
2026-05-29 17:39:23 +0000

    refactor: setup modular express architecture with middleware layering

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,22 @@
 import express, { json } from "express";
 import errorMiddleware from "./middlewares/error.middleware.js";
-import pageNotFoundMiddleware from "./middlewares/pageNotFound.middleware.js";
+import pageNotFoundMiddleware from "./middlewares/page-not-found.middleware.js";
+import router from "./routes/index.routes.js";
 
 const app = express();
-
 app.use(json());
 
-app.use("/health", (req, res) => {
-  res.status(200).json({ success: true, message: "api is running normally" });
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to Authentication API");
 });
 
-app.use("", pageNotFoundMiddleware);
+app.get("/health", (req, res) => {
+  res.status(200).json({ success: true, message: "api running normally" });
+});
+
+app.use("/api/v1", router);
+
+app.use(pageNotFoundMiddleware);
 app.use(errorMiddleware);
 
 export default app;
